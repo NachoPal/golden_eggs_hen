a7ea5d7c993e050a7c23a4da1100fb6705f130d8
Nacho <ignacio.palacios.santos@gmail.com>
2017-06-21 19:51:36 +0800

    Adding connection to Bittrex API

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'bittrex'
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +11,11 @@ Bundler.require(*Rails.groups)
 
 module GoldenEggsHen
   class Application < Rails::Application
+
+    Bittrex.config do |c|
+      c.key = ENV["BITTREX_API_KEY"]
+      c.secret = ENV["BITTREX_API_SECRET"]
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
